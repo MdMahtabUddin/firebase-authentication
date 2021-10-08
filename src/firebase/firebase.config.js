0bcd0223795f3824cb08298df28f773ede698f70
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2021-10-08 23:52:08 +0600

    initial commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC3G1OH_kqSBKWL2owCqQiD6Gnjfaegd2c",
+  authDomain: "simple-firebase-95511.firebaseapp.com",
+  projectId: "simple-firebase-95511",
+  storageBucket: "simple-firebase-95511.appspot.com",
+  messagingSenderId: "114579888052",
+  appId: "1:114579888052:web:70c4d9e3f17c83b5dd04e1"
+};
